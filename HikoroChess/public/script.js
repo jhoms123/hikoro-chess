1e9e98a30b602020446082d7dc56efd9ece0c3d2
jhoms123 <69187735+jhoms123@users.noreply.github.com>
2025-10-27 23:29:26 -0400

    Update script.js

--- HikoroChess/public/script.js
+++ HikoroChess/public/script.js
@@ -1395,8 +1395,6 @@ document.addEventListener('DOMContentLoaded', () => {
             #go-board-container::before {
                 width: ${linesWidth}px;
                 height: ${linesHeight}px;
-                /* top: ${paddingPx}px; */  /* REMOVED */
-                /* left: ${paddingPx}px; */ /* REMOVED */
             }
         `;
         // --- ✅ END: Dynamic Size Adjustments ---
